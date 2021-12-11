008b3c781ecad476ba97d4a6bb5274d2166a4cf9
Ιάσων Φίλιππος Νταγιάννης <csd3540@csd.uoc.gr>
2021-12-11 21:27:08 +0200

    routing v.2

--- fullstack-template/frontend/src/app/app.module.ts
+++ fullstack-template/frontend/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { VotingPhaseComponent } from './pages/Mobile/voting-phase/voting-phase.component';
 import { IdeasComponent } from './pages/Mobile/ideas/ideas.component';
 import { MobileHomeComponent } from './pages/Mobile/mobile-home/mobile-home.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MaterialModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -21,11 +20,10 @@ import { MaterialModule } from '@angular/material';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    MaterialModule,
-    MatButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
  
