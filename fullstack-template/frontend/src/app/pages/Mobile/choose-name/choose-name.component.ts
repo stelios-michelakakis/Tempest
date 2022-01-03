748287dcebf0210361cad4d3c92a0f2a562e99a0
Ιάσων Φίλιππος Νταγιάννης <csd3540@csd.uoc.gr>
2022-01-03 22:28:18 +0200

    fixes

---
+++ fullstack-template/frontend/src/app/pages/Mobile/choose-name/choose-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ami-fullstack-choose-name',
+  templateUrl: './choose-name.component.html',
+  styleUrls: ['./choose-name.component.scss']
+})
+export class ChooseNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
