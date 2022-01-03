8f798ac24f0a6eb5f1e1de2410d416b795728277
Stylianos Michelakakis <csd3524@csd.uoc.gr>
2022-01-03 21:17:04 +0200

    Mobile Join Created

--- fullstack-template/frontend/src/app/app.routing.ts
+++ fullstack-template/frontend/src/app/app.routing.ts
@@ -3,24 +3,27 @@ import { Routes, RouterModule } from '@angular/router';
 import { IdeasComponent } from './pages/Walls/ideaWall/ideaWall.component';
 import { MobileHomeComponent } from './pages/Mobile/mobile-home/mobile-home.component';
 import { VotingPhaseComponent } from './pages/Mobile/voting-phase/voting-phase.component';
+import {JoinComponent} from './pages/Mobile/join/join.component'
+import {CreateComponent} from './pages/Mobile/create/create.component'
 
 
 const routes: Routes = [
   {path:'Mobile', component:MobileHomeComponent,
   children: 
     [
-      {
-        path: 'Mobile/voting-phase', // child route path
-        component: VotingPhaseComponent, // child route component that the router renders
-      }
+      {path: 'Mobile/join', component: JoinComponent,},
+      {path:'Mobile/create', component: CreateComponent,}
     ],
   },
+  {path: 'Mobile/join', component: JoinComponent},
   {path:'Wall/ideaWall', component:IdeasComponent},
   {path:'Mobile/voting-phase', component:VotingPhaseComponent},
+  {path:'Mobile/create', component:CreateComponent},
+
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
   { path: 'socket-events', loadChildren: () => import('./pages/socket-events/socket-events.module').then(m => m.SocketEventsModule) },
   { path: 'tasks', loadChildren: () => import('./pages/tasks/tasks.module').then(m => m.TasksModule) },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'Mobile/create', pathMatch: 'full' },
  
 ];
 
