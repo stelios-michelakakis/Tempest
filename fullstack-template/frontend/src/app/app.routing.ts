8cd954fff58d1a3852210d0cff4a00279f1177ae
Stylianos Michelakakis <csd3524@csd.uoc.gr>
2022-01-05 00:16:38 +0200

    done

--- fullstack-template/frontend/src/app/app.routing.ts
+++ fullstack-template/frontend/src/app/app.routing.ts
@@ -5,6 +5,9 @@ import { MobileHomeComponent } from './pages/Mobile/mobile-home/mobile-home.comp
 import { VotingPhaseComponent } from './pages/Mobile/voting-phase/voting-phase.component';
 import {JoinComponent} from './pages/Mobile/join/join.component';
 import {CreateComponent} from './pages/Mobile/create/create.component';
+import {CreateModesComponent} from './pages/Mobile/create-modes/create-modes.component';
+import {CreateTimerComponent} from './pages/Mobile/create-timer/create-timer.component';
+import {CreateOverviewComponent} from './pages/Mobile/create-overview/create-overview.component';
 
 const routes: Routes = [
   {path:'Mobile', component:MobileHomeComponent,
@@ -17,17 +20,15 @@ const routes: Routes = [
   {path: 'Mobile/join', component: JoinComponent},
   {path:'Wall/ideaWall', component:IdeasComponent},
   {path:'Mobile/voting-phase', component:VotingPhaseComponent},
-  {path:'Mobile/create', component:CreateComponent,
-  /*children: 
-    [
-      {path:'Mobile/create-modes', component:CreateModesComponent},
-    ]
-  */},
+  {path:'Mobile/create', component:CreateComponent},
+  {path:'Mobile/create-modes', component:CreateModesComponent},
+  {path:'Mobile/create-timer', component:CreateTimerComponent},
+  {path:'Mobile/create-overview', component:CreateOverviewComponent},
 
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
   { path: 'socket-events', loadChildren: () => import('./pages/socket-events/socket-events.module').then(m => m.SocketEventsModule) },
   { path: 'tasks', loadChildren: () => import('./pages/tasks/tasks.module').then(m => m.TasksModule) },
-  { path: '**', redirectTo: 'Mobile/create', pathMatch: 'full' },
+  { path: '**', redirectTo: 'Mobile/create-overview', pathMatch: 'full' },
  
 ];
 
